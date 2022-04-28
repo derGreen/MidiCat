69f29665ffc2add75e1a4f769c46d632399f7b02
derGreen <dergreen@gmail.com>
2022-04-28 23:48:26 +0200

    add README

--- MidiCat.cpp
+++ MidiCat.cpp
@@ -13,8 +13,8 @@ int main() {
     int velocity;
     
     char usrChannel;
-    int usrOctaveShift = 12;
-    string usrPath = "evangelion.csv";
+    int usrOctaveShift = 1;
+    string usrPath;
     
     const char *marimba[36] = {"vq","vq#","vw","vw#","ve","vr","vr#","vt","vt#","vz","vz#","vu","q","q#","w","w#","e","r","r#","t","t#","z","z#","u","^q","^q#","^w","^w#","^e","^r","^r#","^t","^t#","^z","^z#","^u"};
     const char *Piano[36] = {"v1","v1#","v2","v2#","v3","v4","v4#","v5","v5#","v6","v6#","v7","1","1#","2","2#","3","4","4#","5","5#","6","6#","7","^1","^1#","^2","^2#","^3","^4","^4#","^5","^5#","^6","^6#","^7"};
@@ -32,24 +32,21 @@ int main() {
     cout << "Channel?";
     cin >> usrChannel;
 
-    cout << "ocrave shift? (dafault 0, use if high notes roll over to low notes)";
+    cout << "ocrave shift? (dafault 1, use if high notes roll over to low notes)";
     cin >> usrOctaveShift;  
 
+    cout << "\n";
     cout << "!bongo+ ";
 
     // Use a while loop together with the getline() function to read the file line by line
     while (getline (userCSV, CSV_Line)) {   
         
-        // get Song velocity, from first Line, last argument
-        
+        // get Song velocity, from first Line, last argument        
         if (CSV_Line.find("Header")!= -1){
         string velocityStr =  CSV_Line.substr(CSV_Line.rfind(",")+2,(CSV_Line.length()-CSV_Line.rfind(",")+2));        
         velocity = stoi(velocityStr);        
         }
         
-
-        
-
         if (CSV_Line.find(usrChannel) == 0) { //nur Zeilen mit Channel in pos 0 
 
             if(CSV_Line.find("Note_on") != -1){ // nur Zeilen in denen ein Note_on command steht. 
@@ -76,9 +73,9 @@ int main() {
                 int catTime = midiTimeInt/velocity;
 
                 //cretae Cat readable Notes
-                //#### PIANO Version! 
+                //.... PIANO Version! 
                 //string catNote = Piano[(midiNoteInt % 36)]; 
-                //#### MARIMBA Version! 
+                //.... MARIMBA Version! 
                 string catNote = marimba[(midiNoteInt % 36)]; 
 
                 //Build Song
@@ -93,16 +90,11 @@ int main() {
                 }
                 
                 lastTime = catTime;
-            } 
+            }
             
-        }  
+        } 
         
     }
-
-    /* Midi line manipulation: 
-    2,       509952, Note_off_c, 0,     48,   0
-    Channel, Time,   Cmd,        param, Note, velocit
-    */
 
 // Close the file
 userCSV.close();
